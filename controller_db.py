e8d86a2b67aa09cc8594a6e024d495069800ac93
Miguel <micmigm@gmail.com>
2024-07-06 18:17:10 -0300

    Simplificacion del query SQL de consolidados y borrado de funciones sin utilizar

--- controller_db.py
+++ controller_db.py
@@ -2,59 +2,31 @@
 from db import conectarMySQL
 
 
-# Read -> SELECT
-def obtenerHinchas():
-    # conexion mysql
-    conexion = conectarMySQL()
-    productos = []
-    with conexion.cursor() as cursor:
-        # Create a new record
-        sql = """SELECT * FROM hinchas"""
-        cursor.execute(sql)
-        productos = cursor.fetchall()
-        conexion.commit()
-        conexion.close()
-        return productos
-    
-
     # Read -> SELECT
-def obtenerCopas():
-    # conexion mysql
-    conexion = conectarMySQL()
-    productos = []
-    with conexion.cursor() as cursor:
-        # Create a new record
-        sql = """SELECT * FROM copas"""
-        cursor.execute(sql)
-        productos = cursor.fetchall()
-        conexion.commit()
-        conexion.close()
-        return productos
-
 def obtener_consagraciones_filtradas(era, ambito, competencia_tipo):
     conexion = conectarMySQL()
     resultados = []
     with conexion.cursor() as cursor:
         sql = """
-        SELECT e.nombre, SUM(ce.cantidad) as total
-        FROM consagraciones_equipo ce
-        JOIN equipo e ON ce.equipo = e.id
-        JOIN competencia c ON ce.competencia = c.id
-        WHERE 1=1
+        SELECT equipo.nombre, SUM(consagraciones_equipo.cantidad) as total
+        FROM consagraciones_equipo 
+        JOIN equipo ON consagraciones_equipo.equipo = equipo.id 
+        JOIN competencia ON consagraciones_equipo.competencia = competencia.id
         """
+
         params = []
         
         if era != "0":
-            sql += " AND c.era = %s"
+            sql += " AND competencia.era = %s"
             params.append(era)
         if ambito != "0":
-            sql += " AND c.ambito = %s"
+            sql += " AND competencia.ambito = %s"
             params.append(ambito)
         if competencia_tipo != "0":
-            sql += " AND c.tipo = %s"
+            sql += " AND competencia.tipo = %s"
             params.append(competencia_tipo)
         
-        sql += " GROUP BY e.id ORDER BY total DESC"
+        sql += " GROUP BY equipo.id ORDER BY total DESC"
         
         cursor.execute(sql, params)
         resultados = cursor.fetchall()
